6265e79015b7846e1fab431c5585ae610ff4c321
darioplazaleon <darioplaza02@hotmail.com>
2024-09-30 22:14:38 -0300

    first commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,23 @@
+export default function Navbar() {
+  return (
+    <header className="w-full h-20 bg-gray-800 text-white flex items-center justify-center">
+      <nav className="w-11/12 h-full flex items-center justify-between">
+        <div className="pl-4 flex gap-6">
+          <h1>Courselint</h1>
+          <button>Categories</button>
+        </div>
+        <div className="w-96 text-black">
+          <input
+            type="text"
+            placeholder="Search for anything"
+            className="w-full rounded-lg pl-4 h-10"
+          />
+        </div>
+        <div className="flex gap-4">
+          <button className="border w-20 h-10">Log in</button>
+          <button className="w-20 h-10 bg-white text-black">Sign up</button>
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <main className="w-full h-min-screen">
+      <section className="w-full h-full flex items-center justify-center flex-col">
+        <div className="">
+          <h1>Courselint</h1>
+        </div>
+        <h3>All the skills you need in one place</h3>
+        <p>Check some courses.</p>
+      </section>
+    </main>
+  );
+}
